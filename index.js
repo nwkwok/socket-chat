16cddc97b34f8c98ac0f8457bc3498423a3f24b0
nwkwok <nwkwok@gmail.com>
2021-03-01 21:31:34 -0800

    display messages when input is submitted

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-const express = require('express');
-const app = express();
+const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+// io object creates a new instance passing in http
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
+
+
+// listens on connection event for incoming sockets.
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
 
 http.listen(3000, () => {
     console.log("Listening on port 3000");
